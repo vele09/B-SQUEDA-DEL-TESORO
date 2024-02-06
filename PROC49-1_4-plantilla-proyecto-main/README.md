fbb54348f5158e5571841ec56e5b34aea556a439
vele09 <144856539+vele09@users.noreply.github.com>
2024-02-06 17:01:04 -0600

    Add files via upload

---
+++ PROC49-1_4-plantilla-proyecto-main/README.md
@@ -0,0 +1,8 @@
+# PROC49-1_4-plantilla-proyecto
+Plantilla del alumno.  
+Búsqueda del tesoro. Etapa 1.  
+Modificación por parte del alumno de 2 comentarios.  
+  
+- Proyect Booster
+  
+### Texto en inglés: Treasure-hunt-template
---
+++ PROC49-1_4-plantilla-proyecto-main/js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCIÓN";
+const accessCode3 = "BASE DE DATOS";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Pista: ¡Siempre cambiando, no es constante!", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("F N I N U C Ó", 700,150)
+    fill("lightblue")
+    text("Pista: ¡Realiza una tarea particular!", 700,170)
+
+    fill("white")
+    textSize(15)
+    text("B S D D T S A E E A O", 100,250)
+    fill("lightblue")
+    text("Pista: ¡Almacena toda la información!", 100,270)
+
+}
